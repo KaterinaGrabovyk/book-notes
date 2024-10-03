63ca15732de744570d9151adbcea95cb43eb4178
KaterinaGrabovyk <katya.grabovik@ukr.net>
2024-10-03 23:29:34 +0300

    new add functional. isbn handler. book cards

--- index.js
+++ index.js
@@ -33,11 +33,57 @@ app.get("/", async (req, res) => {
 app.get("/add", async (req, res) => {
   res.render("add.ejs");
 });
+
+function isValidISBN10(isbn) {
+  let sum = 0;
+  for (let i = 0; i < 9; i++) {
+    sum += (10 - i) * isbn[i];
+  }
+  const checkDigit = isbn[9].toUpperCase() === "X" ? 10 : isbn[9];
+  sum += checkDigit;
+  return sum % 11 === 0;
+}
+
+function isValidISBN13(isbn) {
+  let sum = 0;
+  for (let i = 0; i < 12; i++) {
+    sum += (i % 2 === 0 ? 1 : 3) * isbn[i];
+  }
+  const checkDigit = (10 - (sum % 10)) % 10;
+  return checkDigit === isbn[12];
+}
+
+function isValidISBN(isbn) {
+  if (isbn.length === 10) {
+    return isValidISBN10(isbn);
+  }
+  if (isbn.length === 13) {
+    return isValidISBN13(isbn);
+  }
+  return false;
+}
+
 app.post("/add", async (req, res) => {
   try {
+    if (!isValidISBN(req.body.isbn)) {
+      res.render("add.ejs", {
+        errorISBN: "wrong ISBN code",
+      });
+    }
+    const cover = await axios.get(
+      `https://bookcover.longitood.com/bookcover/${req.body.isbn}`
+    );
+    console.log(cover.data.url);
     await db.query(
-      "INSERT INTO booknotes (title,author,notedate,note) VALUES ($1,$2,NOW(),$3)",
-      [req.body.title, req.body.author, req.body.note]
+      "INSERT INTO booknotes (title,author,notedate,note,rating,isbn,cover) VALUES ($1,$2,NOW(),$3,$4,$5,$6)",
+      [
+        req.body.title,
+        req.body.author,
+        req.body.note,
+        req.body.rating,
+        req.body.isbn,
+        cover.data.url,
+      ]
     );
     res.redirect("/");
   } catch (err) {
