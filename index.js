36ce1d8ca8c97238c7efe3d7f01e500d84a49e2b
KaterinaGrabovyk <katya.grabovik@ukr.net>
2024-10-04 22:41:07 +0300

    Update function,error handlers

--- index.js
+++ index.js
@@ -56,64 +56,83 @@ app.get("/add", async (req, res) => {
   res.render("add.ejs");
 });
 //functions for handling ISBN codes
-function isValidISBN10(isbn) {
-  let sum = 0;
-  for (let i = 0; i < 9; i++) {
-    sum += (10 - i) * isbn[i];
-  }
-  const checkDigit = isbn[9].toUpperCase() === "X" ? 10 : isbn[9];
-  sum += checkDigit;
-  return sum % 11 === 0;
-}
-
-function isValidISBN13(isbn) {
-  let sum = 0;
-  for (let i = 0; i < 12; i++) {
-    sum += (i % 2 === 0 ? 1 : 3) * isbn[i];
-  }
-  const checkDigit = (10 - (sum % 10)) % 10;
-  return checkDigit === isbn[12];
-}
-
 function isValidISBN(isbn) {
-  if (isbn.length === 10) {
-    return isValidISBN10(isbn);
+  if (isbn.length === 10 || isbn.length === 13) {
+    return true;
+  } else {
+    return false;
   }
-  if (isbn.length === 13) {
-    return isValidISBN13(isbn);
-  }
-  return false;
 }
 //Adds new book review
 app.post("/add", async (req, res) => {
   try {
-    if (!isValidISBN(req.body.isbn)) {
+    if (isValidISBN(req.body.isbn) === false) {
       res.render("add.ejs", {
         errorISBN: "wrong ISBN code",
       });
+    } else {
+      const cover = await axios.get(
+        `https://bookcover.longitood.com/bookcover/${req.body.isbn}`
+      );
+      console.log(cover.data.url);
+      await db.query(
+        "INSERT INTO booknotes (title,author,notedate,note,rating,isbn,cover,updated) VALUES ($1,$2,NOW(),$3,$4,$5,$6,NOW())",
+        [
+          req.body.title,
+          req.body.author,
+          req.body.note,
+          req.body.rating,
+          req.body.isbn,
+          cover.data.url,
+        ]
+      );
+      res.redirect("/");
     }
-    const cover = await axios.get(
-      `https://bookcover.longitood.com/bookcover/${req.body.isbn}`
-    );
-    console.log(cover.data.url);
-    await db.query(
-      "INSERT INTO booknotes (title,author,notedate,note,rating,isbn,cover,updated) VALUES ($1,$2,NOW(),$3,$4,$5,$6,NOW())",
-      [
-        req.body.title,
-        req.body.author,
-        req.body.note,
-        req.body.rating,
-        req.body.isbn,
-        cover.data.url,
-      ]
-    );
-    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Update
+app.get("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const b = await db.query(`SELECT * FROM booknotes WHERE id=${id}`);
+  res.render("update.ejs", {
+    book: b.rows[0],
+  });
+});
+app.post("/update/:id", async (req, res) => {
+  try {
+    if (isValidISBN(req.body.isbn) === false) {
+      const id = req.params.id;
+      const b = await db.query(`SELECT * FROM booknotes WHERE id=${id}`);
+      res.render("update.ejs", {
+        book: b.rows[0],
+        errorISBN: "Previous ISBN code was wrong. Data wasn't updated",
+      });
+    } else {
+      const cover = await axios.get(
+        `https://bookcover.longitood.com/bookcover/${req.body.isbn}`
+      );
+      await db.query(
+        `UPDATE booknotes SET title = ($1),author = ($2),note = ($3),rating = ($4),isbn = ($5),cover = ($6),updated = NOW() WHERE id = ${req.params.id}`,
+        [
+          req.body.title,
+          req.body.author,
+          req.body.note,
+          req.body.rating,
+          req.body.isbn,
+          cover.data.url,
+        ]
+      );
+      res.redirect("/");
+    }
   } catch (err) {
     console.log(err);
   }
 });
 //Deletes
-app.post("/delete/:id", async (req, res) => {
+app.get("/delete/:id", async (req, res) => {
   try {
     const id = req.params.id;
     await db.query(`DELETE FROM booknotes WHERE id=${id}`);
